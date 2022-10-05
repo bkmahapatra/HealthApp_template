c63e8f278d6a4999e0ff70f1907359470486a097
bkmahapatra <bkmahapatra27@gmail.com>
2022-10-05 17:37:29 +0530

    fix3

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ nav_items.forEach(item=>{
         item.classList.add("showbar")
     })
 })
+
+///menubtn
+const menubtn=document.querySelector(".menu__btn");
+const navclose=document.querySelector(".menu__close")
+const navmenu=document.querySelector(".nav__menu");
+
+menubtn.addEventListener("click",()=>{
+    navmenu.classList.add("menu__show");
+})
+navclose.addEventListener('click',()=>{
+    navmenu.classList.remove("menu__show");
+})
